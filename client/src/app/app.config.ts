6b25f68237d65d3aa5f230e3b5cfacdd43fe1164
Abhishek Pandey <abhishek.pandey@exazeit.com>
2026-04-20 16:05:23 +0530

    Headers,Components,interfaces,css

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import 'zone.js'
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZoneChangeDetection()
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    provideHttpClient()
   ]
 };
